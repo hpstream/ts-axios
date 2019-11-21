7254e3f1c6bec56c480a9e48a3bdaff785cdd6e7
hpstream <915414300@qq.com>
2019-11-21 17:27:29 +0800

    feat: 测试提交数据

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,6 @@ function axios(config: AxiosRequestConfig): void {
   handelData(config)
   xhr(config)
 }
-let s = { a: 2, 2: 2 }
-
-console.log(s)
 function processConfig(config: AxiosRequestConfig): void {
   // 处理url
   config.url = handelUrl(config)
